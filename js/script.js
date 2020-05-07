3832f1893defffac333f34bff2da5c1980a33c22
Matheus Barbosa <mattbarb93@gmail.com>
2020-05-07 19:31:12 -0400

    progress on reworking the mobile navbar

--- js/script.js
+++ js/script.js
@@ -2,49 +2,49 @@ $(document).ready(function() {
     
     /*Display either Desktop version or Mobile Version when first visiting the site */
     
-    $( window ).width(function() {
-        if($(window).width() <=751){
-            $('nav').addClass("sticky");
-        } 
-        else $('nav').removeClass("sticky");
-      }),
+    // $( window ).width(function() {
+    //     if($(window).width() <=751){
+    //         $('nav').addClass("sticky");
+    //     } 
+    //     else $('nav').removeClass("sticky");
+    //   }),
 
     /* For the sticky navigation to appear whenever the media query hits */
 
     /*PS: I have no idea why it only applies 17px below the resolution I set to. Go back to check that out later */
     
-    $( window ).resize(function() {
-        if($(window).width() <=751){
-            $('nav').addClass("sticky");
-        } 
-        else $('nav').removeClass("sticky");
-      }),
+    // $( window ).resize(function() {
+    //     if($(window).width() <=751){
+    //         $('nav').addClass("sticky");
+    //     } 
+    //     else $('nav').removeClass("sticky");
+    //   }),
 
 
     /* On Click of Menu button, display the menu options */
       $(".js--nav-icon").click(function() {
 
-
-
+        const nav = document.querySelector('#Main-Nav')
         const hamburgerMenu = document.querySelector('ion-icon')
+        const mainMenu = document.querySelector('.main-nav');
 
         //Change ion icon based on the user's click. If menu is open, the icon will be an X, so the user can close it.
 
         if(hamburgerMenu.name === "menu-outline"){
-          hamburgerMenu.name = "close-outline"
+          hamburgerMenu.name = "close-outline";
+          nav.style.display = 'block';
+          mainMenu.style.display = 'block';
         }
 
         else{
-          hamburgerMenu.name = "menu-outline"
+          hamburgerMenu.name = "menu-outline";
+          nav.style.display = "none";
+          mainMenu.style.display = "none";
         }
+
+
         
-
-
-
-
-          let nav = document.getElementById()
-
-          nav.toggle(300);
+        
 
 
 
