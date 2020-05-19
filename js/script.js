1eeed267f11e37815435b6b3abf9ec7441083332
Matheus Barbosa <mattbarb93@gmail.com>
2020-05-18 21:57:14 -0400

    started set up on carousel

--- js/script.js
+++ js/script.js
@@ -1,93 +1,76 @@
-$(document).ready(function() {
-    
-    /*Display either Desktop version or Mobile Version when first visiting the site */
-    
-    // $( window ).width(function() {
-    //     if($(window).width() <=751){
-    //         $('nav').addClass("sticky");
-    //     } 
-    //     else $('nav').removeClass("sticky");
-    //   }),
+$(document).ready(function () {
 
-    // /* For the sticky navigation to appear whenever the media query hits */
+  /*Display either Desktop version or Mobile Version when first visiting the site */
 
-    // /*PS: I have no idea why it only applies 17px below the resolution I set to. Go back to check that out later */
-    
-    // $( window ).resize(function() {
-    //     if($(window).width() <=751){
-    //         $('nav').addClass("sticky");
-    //     } 
-    //     else $('nav').removeClass("sticky");
-    //   }),
+  // $( window ).width(function() {
+  //     if($(window).width() <=751){
+  //         $('nav').addClass("sticky");
+  //     } 
+  //     else $('nav').removeClass("sticky");
+  //   }),
 
+  // /* For the sticky navigation to appear whenever the media query hits */
 
-    /* On Click of Menu button, display the menu options */
+  // /*PS: I have no idea why it only applies 17px below the resolution I set to. Go back to check that out later */
 
-      
-      $(".icon-right").click(function() {
-
-        const hamburgerMenu = document.querySelector('ion-icon');
-
-        const hiddenMenu = document.querySelector('.hidden-menu')
+  // $( window ).resize(function() {
+  //     if($(window).width() <=751){
+  //         $('nav').addClass("sticky");
+  //     } 
+  //     else $('nav').removeClass("sticky");
+  //   }),
 
 
-        //Change ion icon based on the user's click. If menu is open, the icon will be an X, so the user can close it.
+  /* On Click of Menu button, display the menu options */
 
-        if(hamburgerMenu.name === "menu-outline"){
-          hamburgerMenu.name = "close-outline",
-          hiddenMenu.style.display = 'inherit';
-          
 
-          
-        }
+  $(".icon-right").click(function () {
 
-        else{
-          hamburgerMenu.name = "menu-outline";
-          
-          hiddenMenu.style.display = 'none';
+    const hamburgerMenu = document.querySelector('ion-icon');
 
-        }
+    const hiddenMenu = document.querySelector('.hidden-menu')
+
+
+    //Change ion icon based on the user's click. If menu is open, the icon will be an X, so the user can close it.
+
+    if (hamburgerMenu.name === "menu-outline") {
+      hamburgerMenu.name = "close-outline",
+        hiddenMenu.style.display = 'inherit';
 
 
 
-        
-        
+    }
 
+    else {
+      hamburgerMenu.name = "menu-outline";
 
+      hiddenMenu.style.display = 'none';
 
+    }
+    // let nav = $("#Main-Nav");  
+    // let icon = $(".js--nav-icon i");
 
+    // nav.toggle(300);
 
-
-
-
-
-
-
-
-        // let nav = $("#Main-Nav");  
-        // let icon = $(".js--nav-icon i");
-
-        // nav.toggle(300);
-        
-        // /* On Mobile nav, X button appears after clicking on the Menu button  */ 
-        // if (icon.hasClass('md hydrated')) {
-        //     icon.addClass('ion-close-round');
-        //     icon.removeClass('md hydrated');
-        // } else {
-        //     icon.addClass('ion-navicon-round');
-        //     icon.removeClass('ion-close-round');
-        // }        
-      }),
+    // /* On Mobile nav, X button appears after clicking on the Menu button  */ 
+    // if (icon.hasClass('md hydrated')) {
+    //     icon.addClass('ion-close-round');
+    //     icon.removeClass('md hydrated');
+    // } else {
+    //     icon.addClass('ion-navicon-round');
+    //     icon.removeClass('ion-close-round');
+    // }        
+  }),
 
     /* On Pizza Popup, carrosel with fade effect to switch slides */
     $('.popup-carousel').slick({
     });
-    /* Navigation scroll */
-    
-    
-    /* Animations on scroll */
-    
-    
-    /* Mobile navigation */
-   
+  /* Navigation scroll */
+
+
+  /* Animations on scroll */
+
+
+  /* Mobile navigation */
+
 });
